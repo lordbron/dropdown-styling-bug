81a560ea30df3c84b574065281d779646a62e0c0
lordbron <noreply>
2016-06-09 19:02:48 +0000

    Add component sub1

---
+++ ui/sub1.reel/sub1.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/sub1.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Sub1
+ * @extends Component
+ */
+exports.Sub1 = Component.specialize(/** @lends Sub1# */ {
+    constructor: {
+        value: function Sub1() {
+            this.super();
+        }
+    }
+});
